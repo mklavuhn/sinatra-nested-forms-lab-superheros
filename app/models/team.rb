833fa7a5c81d93693f76741e232a9d0bf94c427e
mklavuhn <michaelklavuhn3@gmail.com>
2022-02-16 20:32:34 -0800

    commit tree aba163f5159c6b5dc21e50d23f933b156a2d5178

---
+++ app/models/team.rb
@@ -0,0 +1,17 @@
+class Team 
+
+    attr_accessor :name, :motto
+
+    TEAM = []
+
+    def initialize(params)
+        @name = params[:name]
+        @motto = params[:motto]
+        TEAM << self
+    end
+
+    def self.all
+        TEAM
+    end
+
+end
